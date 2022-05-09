43e46e9222bc6bbdec4a9ba9d4e39d818f6f2354
Robin Delbaere <hello@rdelbaere.fr>
2022-05-09 14:20:28 +0200

    Add query for library

--- library.php
+++ library.php
@@ -2,6 +2,22 @@
 require_once './components/data.php';
 require_once './components/functions.php';
 require_once './components/header.php';
+
+$query = <<<SQL
+    SELECT 
+        game.id, game.title, game.poster, 
+        GROUP_CONCAT(genre.name SEPARATOR " - ") AS genre, 
+        COUNT(review.game_id) AS counterRecommandation 
+    FROM game 
+        LEFT JOIN review ON game.id = review.game_id AND review.is_recommanded = 1
+        LEFT JOIN game_genre ON game.id = game_genre.game_id
+        LEFT JOIN genre ON game_genre.genre_id = genre.id
+    GROUP BY game.id;
+SQL;
+
+
+$stmt = $db->query($query);
+$games = $stmt->fetchAll();
 ?>
 
 <h1>Bibliothéque</h1>
@@ -10,12 +26,13 @@ require_once './components/header.php';
     <?php foreach($games as $game){ ?>
         <a href="/single.php?id=<?php echo $game['id']; ?>" class="card">
             <div class="card-badge">
-                <?php echo average($game['reviews']); ?>
+                <i class="fas fa-thumbs-up"></i>
+                <?php echo $game['counterRecommandation']; ?>
             </div>
-            <img src="<?php echo $game['poster']; ?>" alt="<?php echo $game['name']; ?>" class="card-image">
+            <img src="<?php echo $game['poster']; ?>" alt="<?php echo $game['title']; ?>" class="card-image">
             <div class="card-title">
-                <h2><?php echo $game['name']; ?></h2>
-                <span><?php echo $game['category']; ?></span>
+                <h2><?php echo $game['title']; ?></h2>
+                <span><?php echo $game['genre']; ?></span>
             </div>
         </a>
     <?php } ?>
