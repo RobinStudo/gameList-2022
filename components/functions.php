686829425a9258fcb3022e34f8c3da2f5c7ffb0b
Robin Delbaere <hello@rdelbaere.fr>
2022-05-10 14:14:22 +0200

    Move single query to functions file

--- components/functions.php
+++ components/functions.php
@@ -27,6 +27,42 @@ function findGames(): array
     return $stmt->fetchAll();
 }
 
+function findGame(int $id): ?array
+{
+    global $db;
+
+    $query = <<<SQL
+        SELECT 
+            game.id, game.title, game.poster, game.released_at, game.description,
+            IFNULL(GROUP_CONCAT(DISTINCT genre.name SEPARATOR " - "), "Non connu") AS genre,
+            company_editor.name AS editor,
+            GROUP_CONCAT(DISTINCT company_developer.name SEPARATOR " - ") AS developer,
+            GROUP_CONCAT(DISTINCT platform.name SEPARATOR " - ") AS platforms,
+            COUNT(review.game_id) AS counterRecommandation
+        FROM game
+            LEFT JOIN review ON game.id = review.game_id AND review.is_recommanded = 1
+            LEFT JOIN game_genre ON game.id = game_genre.game_id
+            LEFT JOIN genre ON game_genre.genre_id = genre.id
+            LEFT JOIN company company_editor ON game.editor_id = company_editor.id
+            LEFT JOIN developer ON game.id = developer.game_id
+            LEFT JOIN company company_developer ON developer.company_id = company_developer.id
+            LEFT JOIN game_platform ON game.id = game_platform.game_id
+            LEFT JOIN platform ON game_platform.platform_id = platform.id
+        WHERE game.id = :id
+        GROUP BY game.id;
+    SQL;
+
+    $stmt = $db->prepare($query);
+    $stmt->bindValue('id', $id);
+    $stmt->execute();
+    $game = $stmt->fetch();
+    if($game === false){
+        return null;
+    }
+
+    return $game;
+}
+
 // ----- Utils -----
 
 function getDefaultGamePoster(): string
